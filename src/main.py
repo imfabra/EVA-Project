168e981e5c52044db191b337d8739b23d0800be9
EVA <ing.cristian.roac@gmail.com>
2023-05-16 10:08:10 -0400

    pseudo programa para pruebas

--- src/main.py
+++ src/main.py
@@ -1,13 +1,43 @@
 from lib.rmdx_funtions import RMDX 
 from Interprete.decoder import deco
+import os
+
+
+def send_pos():
+    value_input = input("ingrese angulo: ")
+
+def send_speed():
+    value_input = input("ingrese velcidad: ")
+
+def read_encoder():
+    os.system('sudo /sbin/ip link set can0 down')
+
+def menu():
+    print("1. Enviar Posicion")
+    print("2. Enviar Velocidad")
+    print("3. Leer Posicion")
+
+options = {
+    "1" : send_pos,
+    "2" : send_speed,
+    "3" : read_encoder
+}
 
 if __name__ == "__main__":
-    rmdx = RMDX()
-    rmdx.setup()
-    data = [0x64,0x64,0x64,0x64]
-    motor_id = 0x142
-    res = rmdx.setSpeedClosedLoop(motor_id,data)
-    print(res)
+    while True:
+        menu()
+        option = input("Seleccione una opcion: ")
+        if option == "4":
+            break
+        action = options.get(option)
+        if action:
+            action()
+        else:
+            print("Opcion no valida, seleccione una opcion del menu")
+
+
+
+
 
 
 
