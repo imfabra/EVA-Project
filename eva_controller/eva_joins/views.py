b413de63eed664fdb7420651865d2453d7082576
imfabra <daniel_fabra@outlook.com>
2023-05-21 17:27:51 -0500

    Experimentando controles de rango y estilos CSS

--- eva_controller/eva_joins/views.py
+++ eva_controller/eva_joins/views.py
@@ -1,15 +1,11 @@
+
 from django.shortcuts import render
 
 def joins(request):
-    if 'counter' not in request.session:
-        request.session['counter'] = 0
-
     if request.method == 'POST':
-        if 'increment' in request.POST:
-            request.session['counter'] += 1
-        elif 'decrement' in request.POST:
-            request.session['counter'] -= 1
-
-    context = {'number': '123', 'counter': request.session['counter']}
-
-    return render(request, 'eva_joins/joins.html', context)
+        values = []
+        for i in range(5):
+            value = request.POST.get('range{}'.format(i))
+            values.append(value)
+        return render(request, 'eva_joins/joins.html', {'values': values})
+    return render(request, 'eva_joins/joins.html')
