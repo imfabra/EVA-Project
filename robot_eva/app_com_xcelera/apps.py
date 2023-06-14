89781cbe5c808785ecc01c651ed4b2439c3a3a0b
CrisFRC <ing.cristian.roac@gmail.com>
2023-06-13 21:56:23 -0500

    preliminar view.py

--- robot_eva/app_com_xcelera/apps.py
+++ robot_eva/app_com_xcelera/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AppComXceleraConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_com_Xcelera'
+    name = 'app_com_xcelera'
--- robot_eva/app_com_xcelera/views.py
+++ robot_eva/app_com_xcelera/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from robot_eva.app_crud.models import *
+from robot_eva.app_crud.forms import *
 
 # Create your views here.
+# def motion_robot_details(request):
+#     if request.method == 'POST':
+# la idea aqui es hacer la siguiente sentencia:
+# SELECT  Movimientos.idEtiqueta, Etiquetas.nombre,Movimientos.idMovimiento,
+# Submovimientos.join1, Submovimientos.join2, Submovimientos.join3, Submovimientos.join4, Submovimientos.join5, Submovimientos.velocidad,
+# Submovimientos.tiempo, Submovimientos.orden, Movimientos.descripcion
+# FROM Movimientos
+# INNER JOIN Etiquetas ON Movimientos.idEtiqueta = Etiquetas.idEtiqueta
+# INNER JOIN Submovimientos ON Movimientos.idMovimiento = Submovimientos.idMovimiento;
+
