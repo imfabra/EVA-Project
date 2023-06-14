30c86ebad42c26ea90618fff13b3a6871a0a522b
Cristian Roa <cfroa@bancolombia.com.co>
2023-06-14 09:27:35 -0500

    rr

--- robot_eva/robot_eva/urls.py
+++ robot_eva/robot_eva/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app_crud.urls')),
+    path('', include('app_com_xcelera.urls')),
 ]
