aa8b44baf91ac42f5ea0834ced07e377dc85925c
CrisFRC <ing.cristian.roac@gmail.com>
2023-06-18 13:24:10 -0500

    creado nuevo modelo

--- robot_eva/app_com_xcelera/models.py
+++ robot_eva/app_com_xcelera/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
 # Create your models here.
+class Etiquetas(models.Model):
+    id_etiqueta = models.BigAutoField(primary_key=True)
+    nombre = models.CharField(max_length=45)
+    descripcion = models.CharField(max_length=45)
+
+
+class Movimientos(models.Model):
+    id_movimieto = models.BigAutoField(primary_key=True),
+    id_etiqueta = models.ForeignKey(Etiquetas, on_delete=models.CASCADE,verbose_name="Etiqueta relacionada")
+    descripcion = models.CharField(max_length=45)
+    orden = models.IntegerField()
+
+
+class Submovimientos(models.Model):
+    id_submovimiento = models.BigAutoField(primary_key=True)
+    # id_movimiento = models.ForeignKey(Movimientos, on_delete=models.CASCADE)
+    id_movimiento = models.ManyToManyField(Movimientos,on_delete=models.CASCADE, verbose_name="Movimiento relacionado")
+    join_1 = models.DecimalField()
+    join_2 = models.DecimalField()
+    join_3 = models.DecimalField()
+    join_4 = models.DecimalField()
+    join_5 = models.DecimalField()
+    velocidad = models.DecimalField()
--- robot_eva/robot_eva/settings.py
+++ robot_eva/robot_eva/settings.py
@@ -75,10 +75,32 @@ WSGI_APPLICATION = 'robot_eva.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Sql server
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'EVA',
+#         'USER': 'user',
+#         'PASSWORD': 'contraseña',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'EVA',
+        'USER': 'postgres',
+        'PASSWORD': 'Cristian123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
